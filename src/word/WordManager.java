49ddcd74e78e6ff27a5560cc4b99476506475dbf
inbaea <49175916+inbaea@users.noreply.github.com>
2022-09-04 20:22:32 +0900

    변수 이름 수정

--- src/word/WordManager.java
+++ src/word/WordManager.java
@@ -29,12 +29,12 @@ public class WordManager {
 	
 	public void start() {
 		while(true) {
-			int menu = selectMenu();
-			if(menu == 0) break;
-			if(menu == 4) {
+			int menunum = selectMenu();
+			if(menunum == 0) break;
+			if(menunum == 4) {
 				wordCRUD.addWord();
 			}
-			else if(menu == 1) {
+			else if(menunum == 1) {
 				wordCRUD.listAll();
 			}
 		}
